5eca3e90e6c955156ea9b2906c78b309c23c714f
ampersand22 <jeyulmer@gmail.com>
2023-04-23 11:44:59 -0500

    added wordle

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 import appPost from '../public/assets/projects/appPost.png';
 import polyImg from '../public/assets/projects/polyimg.png'
 import realtorImg from '../public/assets/projects/realtorapp.png'
+import wordleimg from '../public/assets/wordleimg.jpg'
 import ProjectItem from './ProjectItem';
 
 const Projects = () => {
@@ -13,8 +13,14 @@ const Projects = () => {
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Projects
         </p>
-        <h2 className='py-4'>What I&apos;ve Built</h2>
+        <h2 className='py-4'>Let's Build</h2>
         <div className='grid md:grid-cols-2 gap-8'>
+        <ProjectItem
+            title='Wordle-Clone'
+            backgroundImg={wordleimg}
+            projectUrl='https://wordle-x7jz-p0qt1bovl-ampersand22.vercel.app/'
+            tech='React JS'
+          />
           <ProjectItem
             title='App.Post'
             backgroundImg={appPost}
